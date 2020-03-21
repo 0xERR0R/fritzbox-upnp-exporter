bb83222ad758fa040c4472a9c0540c4a3aaeda20
Dimitri Herzog <dimitri.herzog@gmail.com>
2020-03-21 21:56:00 +0100

    digest auth

--- http.go
+++ http.go
@@ -10,7 +10,14 @@ import (
 	"net/http"
 	"strings"
 
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+
+	//digestRequest "github.com/delphinus/go-digest-request"
+
 	log "github.com/sirupsen/logrus"
+	//	dac "github.com/xinsnake/go-http-digest-auth-client"
 )
 
 var dummy = http.Response{
@@ -19,12 +26,23 @@ var dummy = http.Response{
 
 func newRequest(method, uri, body string) *http.Request {
 	request, _ := http.NewRequest(method, uri, strings.NewReader(body))
+	//request.SetBasicAuth("admin", "!jryN18VD!")
+
 	return request
 }
 
 func do(dr *http.Request) io.ReadCloser {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	resp, err := http.DefaultClient.Do(dr)
+
+	//	dr.SetBasicAuth("admin", "!jryN18VD!")
+	//t := digest.NewTransport("admin", "!jryN18VD!")
+
+	//	resp, err := t.RoundTrip(dr)
+
+	resp := digestPost(dr)
+	var err error
+
+	//resp, err := http.DefaultClient.Do(dr)
 	if err != nil {
 		if err, ok := err.(net.Error); ok && err.Timeout() {
 			log.Warn(fmt.Sprintf("Timeout on calling URL %s", dr.URL))
@@ -38,4 +56,129 @@ func do(dr *http.Request) io.ReadCloser {
 		return dummy.Body
 	}
 	return resp.Body
+}
+
+func digestPost(req *http.Request) *http.Response {
+	//   url := host + uri
+	//   method := "POST"
+	//   req, err := http.NewRequest(method, url, nil)
+	//   req.Header.Set("Content-Type", "application/json")
+
+	//	fmt.Printf("request 1 %s", formatRequest(req))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	if resp.StatusCode != http.StatusUnauthorized {
+		log.Printf("Recieved status code '%v' auth skipped", resp.StatusCode)
+		return resp
+	}
+
+	// Authorization: Digest username="admin", realm="HTTPS Access", nonce="BF7D02122AC0FBDF", uri="/upnp/control/lanconfigsecurity", response="31b4b07ab071959270b6870ddad66cd3", qop=auth, nc=00000003, cnonce="f4f08e537623900e", algorithm=MD5
+	defer resp.Body.Close()
+	digestParts := digestParts(resp)
+	digestParts["uri"] = req.URL.Path
+	digestParts["method"] = req.Method
+	//digestParts["realm"] = "F!Box SOAP-Auth"
+	digestParts["username"] = "admin"
+	digestParts["password"] = "!jryN18VD!"
+	body, _ := req.GetBody()
+	l := req.ContentLength
+	action := req.Header.Get("soapaction")
+	req, err = http.NewRequest(req.Method, req.URL.String(), body)
+	req.ContentLength = l
+
+	req.Header.Set("Authorization", getDigestAuthrization(digestParts))
+	req.Header.Set("Content-Type", "text/xml")
+	req.Header.Set("Charset", "utf-8")
+	req.Header.Set("soapaction", action)
+
+	resp, err = client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	//	htmlData, err := ioutil.ReadAll(resp.Body)
+
+	//	fmt.Printf("request 2 %s", formatRequest(req))
+	//	fmt.Printf("reqponse 2 %s", htmlData)
+
+	//   defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		log.Println("response body: ", string(body))
+		return resp
+	}
+	return resp
+}
+
+func formatRequest(r *http.Request) string {
+	// Create return string
+	var request []string
+	// Add the request string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	// Add the host
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	// Loop through headers
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	// If this is a POST, add post data
+	if r.Method == "POST" {
+		r.ParseForm()
+		request = append(request, "\n")
+		request = append(request, r.Form.Encode())
+	}
+	// Return the request as a string
+	return strings.Join(request, "\n")
+}
+
+func digestParts(resp *http.Response) map[string]string {
+	result := map[string]string{}
+	if len(resp.Header["Www-Authenticate"]) > 0 {
+		wantedHeaders := []string{"nonce", "realm", "qop"}
+		responseHeaders := strings.Split(resp.Header["Www-Authenticate"][0], ",")
+		for _, r := range responseHeaders {
+			for _, w := range wantedHeaders {
+				if strings.Contains(r, w) {
+					result[w] = strings.Split(r, `"`)[1]
+				}
+			}
+		}
+	}
+	return result
+}
+
+func getMD5(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func getCnonce() string {
+	b := make([]byte, 8)
+	io.ReadFull(rand.Reader, b)
+	return fmt.Sprintf("%x", b)[:16]
+}
+
+func getDigestAuthrization(digestParts map[string]string) string {
+	d := digestParts
+	ha1 := getMD5(d["username"] + ":" + d["realm"] + ":" + d["password"])
+	ha2 := getMD5(d["method"] + ":" + d["uri"])
+	nonceCount := 00000000
+	cnonce := getCnonce()
+	// Authorization: Digest username="admin", realm="HTTPS Access", nonce="BF7D02122AC0FBDF", uri="/upnp/control/lanconfigsecurity", response="31b4b07ab071959270b6870ddad66cd3", qop=auth, nc=00000003, cnonce="f4f08e537623900e", algorithm=MD5
+	response := getMD5(fmt.Sprintf("%s:%s:%v:%s:%s:%s", ha1, d["nonce"], nonceCount, cnonce, d["qop"], ha2))
+	authorization := fmt.Sprintf(`Digest username="%s", realm="%s", nonce="%s", uri="%s", response="%s", qop="%s", nc="%v", cnonce="%s", algorithm=MD5`,
+		d["username"], d["realm"], d["nonce"], d["uri"], response, d["qop"], nonceCount, cnonce)
+	return authorization
 }
--- upnp.go
+++ upnp.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 const IGDESCENDPOINT string = "/igddesc.xml"
@@ -27,12 +28,23 @@ func (uc *UPnPClient) Execute() map[string]string {
         <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" 
 				s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"> 
             <s:Body><u:%s xmlns:u='%s'/></s:Body>
-        </s:Envelope>`, action.Name, service.ServiceType)
+		</s:Envelope>`, action.Name, service.ServiceType)
 
+			//		fmt.Printf("Calling %s with action %s\n", uc.URL+service.ControlURL, action.Name)
+			//		fmt.Println(message)
+			//dr := newRequest("POST", uc.URL+service.SCPDURL, message)
 			dr := newRequest("POST", uc.URL+service.ControlURL, message)
-			dr.Header.Add("Content-Type", "text/xml; charset='utf-8'")
-			dr.Header.Add("SoapAction", fmt.Sprintf("%s#%s", service.ServiceType, action.Name))
 
+			//dr := dac.NewRequest("admin", "!jryN18VD!", "POST", uc.URL+service.ControlURL, message)
+			dr.Header.Add("Content-Type", "text/xml")
+			dr.Header.Add("charset", "utf-8")
+			dr.Header.Add("SoapAction", fmt.Sprintf("%s#%s", service.ServiceType, action.Name))
+			//dr.Header.Add("Authorization", fmt.Sprintf("Basic %s", "YWRtaW46IWpyeU4xOFZEIQ=="))
+
+			//dr1 := dac.NewRequest("admin", "!jryN18VD!", "POST", uc.URL+service.ControlURL, message)
+			//	response1, err1 := dr1.Execute()
+
+			// TODO close?
 			decoder := xml.NewDecoder(do(dr))
 			for {
 				t, _ := decoder.Token()
@@ -46,7 +58,7 @@ func (uc *UPnPClient) Execute() map[string]string {
 							t, _ = decoder.Token()
 							switch element := t.(type) {
 							case xml.CharData:
-								result[argument.RelatedStateVariable] = string(element)
+								result[service.ServiceId+"_"+argument.RelatedStateVariable] = string(element)
 							}
 						}
 					}
@@ -54,13 +66,29 @@ func (uc *UPnPClient) Execute() map[string]string {
 			}
 		}
 	}
+	printMap(result)
 	return result
+}
+
+func printMap(m map[string]string) {
+	var maxLenKey int
+	for k, _ := range m {
+		if len(k) > maxLenKey {
+			maxLenKey = len(k)
+		}
+	}
+
+	for k, v := range m {
+		fmt.Println(k + ": " + strings.Repeat(" ", maxLenKey-len(k)) + v)
+	}
+	fmt.Println()
 }
 
 func (uc *UPnPClient) parseServices() []Service {
 	services := make([]Service, 0)
 
-	dr := newRequest("GET", uc.URL+IGDESCENDPOINT, "")
+	//	dr := newRequest("GET", uc.URL+IGDESCENDPOINT, "")
+	dr := newRequest("GET", uc.URL+"/tr64desc.xml", "")
 	decoder := xml.NewDecoder(do(dr))
 	for {
 		t, _ := decoder.Token()
@@ -74,8 +102,12 @@ func (uc *UPnPClient) parseServices() []Service {
 				if err := decoder.DecodeElement(&service, &se); err != nil {
 					panic(err)
 				}
+
+				//if strings.Contains(service.ServiceId, "WLANConfiguration") {
 				service.Actions = uc.parseActions(service)
 				services = append(services, service)
+				//}
+
 			}
 		}
 	}
@@ -109,7 +141,7 @@ func (uc *UPnPClient) parseActions(service Service) []Action {
 }
 
 func IsActionGetOnly(action Action) bool {
-	match, _ := regexp.MatchString("^(Get)+[A-z]*", action.Name)
+	match, _ := regexp.MatchString("^(.*Get)+[A-z]*", action.Name)
 	if !match {
 		return false
 	}
